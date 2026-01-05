4e13ecf22d688c0381b8f74bb96851fdc2a42638
Del Motteler <dmotteler@aol.com>
2026-01-05 12:39:31 -0800

    updated events.xml, cheatxml.py

--- picker/cheatxml.py
+++ picker/cheatxml.py
@@ -17,9 +17,8 @@ with open(fn, "r") as fo:
     n2 = html.find('</title>')
     title = html[n + len(ttarg):n2]
     where, when = title.split(" - ")
-    print(when, where)
+    print(f"new event is {when} at { where}")
     newevdt = datetime.strptime(when, "%B %d, %Y %I:%M%p")
-    dtndx = f"{newevdt:%Y%m%d%H%M}"
 
     # isolate the table body (comprised of song rows)
     n = html.find("<tbody")
@@ -50,43 +49,48 @@ with open(fn, "r") as fo:
 
     songlist = ' '.join(songlist)
     qtetSongs = ' '.join(qtetSongs)
-    # print(f'dtndx="{dtndx}" where="{where}" when="{when}" songlist="{songlist}" qtetSongs="{qtetSongs}"')
 
     newevent = {'when': when, 'where':where, 'songlist':songlist, 'qtetSongs':qtetSongs}
 
     # get the current list of events from the git repository and parse it
-    # evfn = os.path.expanduser("~/gitpages/musiclib/events.xml")
-    evfn = os.path.expanduser("/srv/www/htdocs/musiclib/events.xml")
+    evfn = os.path.expanduser("~/gitpages/musiclib/events.xml")
+    # evfn = os.path.expanduser("events.xml")
     tree = ET.parse(evfn)
 
     # make a dict of event dicts, indexed by event yyyymmddhhmm
     eventlist = {}
-    isrepl = None
+    repls = []
     for ev in tree.getroot().findall("event"):
         d = ev.attrib
         # get the dtndx, removing it from the event dict. parse into datetime.
-        dt = datetime.strptime(d.pop('dtndx'), "%Y%m%d%H%M")
+        dtndx = d.pop('dtndx')
+        dt = datetime.strptime(dtndx, "%Y%m%d%H%M")
         eventlist[dt] = d
 
         # if new event is within 4 hours of existing one, assume
         # the time got changed. new event will be added, old one removed.
-        diff = abs(newevdt - dt).seconds
-        if not isrepl and newevdt != dt and abs(newevdt - dt).seconds < 4 * 3600:
-            print(f"new {newevdt} exist {dt} diff {diff}")
-            isrepl = dt
+        diff = abs(newevdt - dt)
+        if diff.days == 0 and diff.seconds > 0 and diff.seconds <= 4 * 3600:
+            repls.append(dt)
 
-    # add new event to event list. this will replace an existing event 
+    print(f"read {len(eventlist)} events from {evfn}")
+
+    # add new event to event list. this will replace an existing event
     # at the same date/time.
     listchgd = False
     if newevdt not in eventlist or eventlist[newevdt] != newevent:
+        if newevdt not in eventlist:
+            print(f"added event at {newevdt}")
+        else:
+            print(f"changed event at {newevdt}")
+
         eventlist[newevdt] = newevent
         listchgd = True
-        print("added event")
 
-    if isrepl:
-        eventlist.pop(isrepl)
+    for dtndx in repls:
+        eventlist.pop(dtndx)
         listchgd = True
-        print(f"removed {isrepl}")
+        print(f"removed event at {dtndx}")
 
     if listchgd:
         # re-create the xml file. rename the old one for backup,
@@ -101,13 +105,12 @@ with open(fn, "r") as fo:
 ''')
             for dt in sorted(eventlist, reverse=True):
                 ev = eventlist[dt]
-                dt = f"{dt:%Y%m%d%H%M}"
-                ev['dtndx'] = dt
+                dtndx = f"{dt:%Y%m%d%H%M}"
+                ev['dtndx'] = dtndx
                 if 'qtetSongs' in ev:
                     fo.write('  <event where="{where}" when="{when}" dtndx="{dtndx}" songlist="{songlist}" qtetSongs="{qtetSongs}" />\n'.format(**ev))
                 else:
                     fo.write('  <event where="{where}" when="{when}" dtndx="{dtndx}" songlist="{songlist}" />\n'.format(**ev))
-
 
             fo.write("</eventlist>\n")
             fo.close()
